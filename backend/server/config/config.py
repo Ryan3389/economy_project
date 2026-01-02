23ae3da3efd3899c97daf61220d24d99c1d3cdf5
Ryan <ryan.cuthbert12@gmail.com>
2026-01-01 21:56:37 -0500

    fixed db connection

--- backend/server/config/config.py
+++ backend/server/config/config.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ENV_PATH = Path(__file__).resolve().parents[2] / ".env"
-print(ENV_PATH)
+ENV_PATH = Path(__file__).resolve().parents[3] / ".env"
 load_dotenv(dotenv_path=ENV_PATH)
-
+print(ENV_PATH)
 fred_api_key = os.getenv("key")
 db = os.getenv("db")
-# print(db)
+
 
 
