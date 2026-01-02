064935e0264c079e4f03ab5e7087804dbf6302af
Ryan <ryan.cuthbert12@gmail.com>
2026-01-01 22:38:21 -0500

    fixed db variables

--- backend/server/config/config.py
+++ backend/server/config/config.py
@@ -1,22 +1,24 @@
+from prefect.variables import Variable
+
+fred_api_key = Variable.get("key")
+db = Variable.get("db")
+
+
+if not db:
+    raise RuntimeError("Prefect variable 'db' is not set")
+
+if not fred_api_key:
+    raise RuntimeError("Prefect variable 'key' not set")
+
 # import os
-# from pathlib import Path
 # from dotenv import load_dotenv
+# from prefect.variables import Variable
 
 
-# ENV_PATH = Path(__file__).resolve().parents[3] / ".env"
-# load_dotenv(dotenv_path=ENV_PATH)
+# load_dotenv()
+
 # fred_api_key = os.getenv("key")
 # db = os.getenv("db")
 
-
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-fred_api_key = os.getenv("key")
-db = os.getenv("db")
-
-if not db:
-    raise RuntimeError("Environment variable 'db' is not set")
+# if not db:
+#     raise RuntimeError("Environment variable 'db' is not set")
